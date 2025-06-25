90bf5461694cef103d6e3e33e38c3c155ec5f742
laaluuzz <laluznicolas@gmail.com>
2025-06-25 17:22:01 -0400

    123

--- frontend/src/js/config.js
+++ frontend/src/js/config.js
@@ -1,6 +1,6 @@
 // Archivo de configuración para endpoints de la API
 
-//const API_BASE_URL = 'https://user-opyf.onrender.com/api';
-const API_BASE_URL = 'http://localhost:9000/api';
+const API_BASE_URL = 'https://user-opyf.onrender.com/api';
+//const API_BASE_URL = 'http://localhost:9000/api';
 // Para usarlo en otros archivos, puedes importar este archivo o incluirlo antes en el HTML
 // Ejemplo de uso: fetch(`${API_BASE_URL}/login`, ...) 
